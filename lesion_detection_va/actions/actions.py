28ad3612af49db3f70325f8e98c32d50c1af8900
Jay Bharadva <b.jay2040@gmail.com>
2022-05-02 19:47:44 -0700

    Custom action example
    Stories -> nlu.yml -> Domain -> endpoints.yml -> Actions.py

--- lesion_detection_va/actions/actions.py
+++ lesion_detection_va/actions/actions.py
@@ -7,10 +7,10 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +25,17 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class ActionRunCNN(Action):
+	def name(self) -> Text:
+		return "action_run_cnn"
+		
+	def run(self, dispatcher: CollectingDispatcher,
+		tracker: Tracker,
+		domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+		dispatcher.utter_message(text="Running Polyp Classifier")
+
+		return []
+	
+	
