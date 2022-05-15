1fb978a16fe2492df2b8220a0fc97b69830b7df7
James Medel <jamesmedel94@gmail.com>
2022-05-14 21:51:49 -0700

    Updated running Powershell script in Python
    Just need to see if I get expected output as powershell script is running.
    
    How do I output results of powershell script to Jupyter notebook? Using Py subprocess.

--- scripts/powershell/build_darknet.ps1
+++ scripts/powershell/build_darknet.ps1
@@ -1,6 +1,9 @@
 # Powershell script uses vcpkg to install darknet: opencv-base, cuda and cudnn
 # for x64-windows; Then goes to darknet/ dir and runs its ps1 script to build it
-cd Notebooks\yolov4\vcpkg
+
+# I am running this script inside Notebooks/ dir from YOLOv4 Notebook
+# so we dont need to include Notebooks/ here
+cd yolov4\vcpkg
 $env:VCPKG_ROOT=$PWD
 .\bootstrap-vcpkg.bat
 # for a quicker install of dependencies
