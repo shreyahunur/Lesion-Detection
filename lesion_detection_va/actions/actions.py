a88667c06325ed96b93f3cfed3ea2380f76f0ea6
James Medel <jamesmedel94@gmail.com>
2022-05-31 09:50:46 -0700

    Tested CNN Polyp Clf Rasa Integration Works
    CNN Polyp Clf Rasa Integration Works. Predicted Polyp Image saved. Rasa terminal reports it as filepath to predicted image.

--- lesion_detection_va/actions/actions.py
+++ lesion_detection_va/actions/actions.py
@@ -9,20 +9,24 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 import os
-# import cv2
+import cv2
 import time
+import random
 import numpy as np
 
 # from typing import Any, Text, Dict, List
 from rasa_sdk import Action #, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.executor import CollectingDispatcher
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # Keras CNN Polyp Classifer
-# from tensorflow.keras.models import load_model
-# import h5py
-# from tensorflow.keras import __version__ as keras_version
+from tensorflow.keras.models import load_model
+import h5py
+from tensorflow.keras import __version__ as keras_version
+
+import xml.etree.ElementTree as ET
+from sklearn.preprocessing import LabelEncoder
 
 # import sys
 
@@ -87,45 +91,102 @@ class ActionRunImageClassification(Action):
 	def name(self):
 		return "action_run_image_classification"
 
+	def get_filepaths(self, basepath, remove_ext=False):
+		files = []
+		filenames = []
+		for filename in os.listdir(basepath):
+			if remove_ext is True:
+				file_name, file_ext = filename.split(".")
+				filepath = os.path.join(basepath, file_name)
+				files.append(filepath)
+				filenames.append(file_name)
+			else:
+				filepath = os.path.join(basepath, filename)
+				files.append(filepath)
+				filenames.append(filename)
+		return files, filenames
+
+	def read_img(self, img_path, flag = cv2.IMREAD_COLOR):
+		# (height, width, 3)
+		image = cv2.imread(img_path, flag)
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		return image
+
+	def get_images(self, image_group, size, flag = cv2.IMREAD_COLOR):
+		images = []
+		for image_path in image_group:
+			image = self.read_img(image_path, flag)
+			resized_img = cv2.resize(image, (size, size))
+			images.append(resized_img)
+		return images
+
+	def get_txt_label_names(self, txt_files):
+		label_names = []
+		for txt_file in txt_files:
+			file = open(txt_file, 'r+')
+			polyp_name = file.readline()
+			label_names.append(polyp_name)
+			file.close()
+		return label_names
+
 	def run(self, dispatcher, tracker, domain):
-		# classes = ["adenomatous", "hyperplastic"]
-		# cnn_file = ".\keras_cnn_clf\jays_polyp_cnn_clf.h5"
+		classes = ["adenomatous", "hyperplastic"]
+		cnn_file = "keras_cnn_clf/polyp_cnn_clf_28000imgs_128hw.h5"
 		patient_id = tracker.get_slot("patient_clf_id")
 		print("Sending polyp classifier for Patient ID =", patient_id)
 
-		# f = h5py.File(cnn_file, mode="r")
-		# model_version = f.attrs.get("keras_version")
-		# keras_veresion = str(keras_version).encode("utf8")
+		f = h5py.File(cnn_file, mode="r")
+		model_version = f.attrs.get("keras_version")
+		keras_veresion = str(keras_version).encode("utf8")
 
-		# if model_version != keras_version:
-		# 	print("You are using Keras version ", keras_version,
-		# 		   ", but the model was built using ", model_version)
+		if model_version != keras_version:
+			print("You are using Keras version ", keras_version,
+				   ", but the model was built using ", model_version)
 
-		# model = load_model(cnn_file)
+		model = load_model(cnn_file)
 
-		# basepath = "test/image/" + patient_id
-		# patient_polyp_img_filenames = os.listdir(basepath)
-		# patient_polyp_img_file = os.path.join(basepath, patient_polyp_img_filenames[0])
-		# patient_polyp_img = cv2.imread(patient_polyp_img_file)
-		# patient_polyp_img = cv2.resize(patient_polyp_img, (120, 120))
-		# patient_polyp_np = np.array(patient_polyp_img)
-		# patient_polyp_np = patient_polyp_np/255
+		base_img_path = "test/image/" + patient_id
 
-		# y_pred = model.predict(patient_polyp_np)
-		# y_pred_polyps = [np.argmax(polyp) for polyp in y_pred]
+		patient_polyp_img_filepaths, patient_polyp_img_filenames = self.get_filepaths(base_img_path)
+		patient_polyp_images = self.get_images(patient_polyp_img_filepaths, 128)
+		patient_polyp_img_nps = np.array(patient_polyp_images)
+		patient_polyp_img_nps = patient_polyp_img_nps/255
 
-		# # center text on image
-		# font = cv2.FONT_HERSHEY_SIMPLEX
-		# polyp_pred_label = "%s: %.2f" % (classes[y_pred_polyps[0]], y_pred)
-		# textsize = cv2.getTextSize(polyp_pred_label, font, 1, 2)[0]
-		# textX = (patient_polyp_img.shape[1] - textsize[0])/2
-		# textY = (patient_polyp_img.shape[0] - textsize[1])/2
-		# cv2.putText(patient_polyp_img, polyp_pred_label, (textX, textY), font, 1, (255,255,255), 2)
-		# out_clf_img_file = "test/clf_img/" + patient_id + "/clf_img_" + patient_polyp_img_filenames[0] + ".jpg"
-		# cv2.imwrite(out_clf_img_file, patient_polyp_img)
+		base_label_path = "test/Annotation/" + patient_id
 
-		# dispatcher.utter_message(image=out_clf_img_file)
-		dispatcher.utter_message(text="Running Img Classifier")
+		patient_polyp_label_filepaths, patient_polyp_label_filenames = self.get_filepaths(base_label_path)
+		patient_polyp_labels = self.get_txt_label_names(patient_polyp_label_filepaths)
+		label_enc = LabelEncoder()
+		patient_polyp_labels = label_enc.fit_transform(patient_polyp_labels)
+
+		# Run CNN Polyp Classification, Save Predicted Img, Utter Pred Image File
+		y_pred = model.predict(patient_polyp_img_nps)
+		y_pred_polyps = [0 if polyp < 0.5 else 1 for polyp in y_pred]
+
+		p_id_frame_n = random.randint(0, len(patient_polyp_images)-1)
+		plt.figure(figsize=(4,4))
+		plt.title("Polyp Classification " + patient_id, fontsize=20)
+		
+		height = patient_polyp_img_nps[p_id_frame_n].shape[0]
+		width = patient_polyp_img_nps[p_id_frame_n].shape[1]
+		plt.text(width/10, height + 8.5,
+			"Actual: {}\nPred: {}".format(
+			classes[patient_polyp_labels[p_id_frame_n]], classes[y_pred_polyps[p_id_frame_n]]),
+			va='top',
+			fontsize=16)
+		plt.axis("off")
+
+		clf_polyp_img = plt.imshow(patient_polyp_img_nps[p_id_frame_n])
+		save_dst="cnn_clf/trained_28000_aug/clf_pred_images"
+
+		if not os.path.exists(save_dst):
+			os.makedirs(save_dst)
+
+		save_clf_polyp_file = "{}/pred_polyp_{}_{}.jpg".format(save_dst, classes[y_pred_polyps[p_id_frame_n]], 0)
+		plt.savefig(save_clf_polyp_file, bbox_inches="tight")
+
+		dispatcher.utter_message(image=save_clf_polyp_file)
+		# dispatcher.utter_message(text="Running Img Classifier")
 		return []
 	    
 
